38d1c6e4d58c25749e241a3890ac5c1586ba163e
Leo <chispainnov@gmail.com>
2026-03-09 14:05:09 -0700

    Browse button addition

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,7 @@ class PagesController < ApplicationController
   def home
     # Homepage shows all approved specimens to convey liveliness
     @featured_taxa = Taxon.ordered_by_latest_approved.limit(10)
+    @specimen_count = SpecimenAsset.where(status: "approved").count
   end
 
   def about
