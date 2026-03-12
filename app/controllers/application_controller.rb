4be285054174dbac035c779892378d7d1baf4ae2
Leo <chispainnov@gmail.com>
2026-03-12 13:01:43 -0700

    Attribution addition in specimen asset

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,12 @@ class ApplicationController < ActionController::Base
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+  private
+
+  def record_not_found
+    redirect_to root_path, alert: "Specimen not found."
+  end
 end
